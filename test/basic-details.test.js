7c587117a10f87ca4417393733561008dd6a932d
Sunita Hembram <sunitahembram.92313@gmail.com>
2022-08-04 06:34:56 +0000

    initial commit

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -2,7 +2,13 @@ import { html, fixture, expect } from '@open-wc/testing';
 import { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
 
-describe('Basic details', () => {
+describe('Basic details', async () => {
   // Write test cases inside this block
   // refer basic-details.js files
+  it('checks form', async () => {
+    const el = await fixture(html `<basic-details></basic-details>`);
+    const form = el.shadowRoot.querySelectorAll('lion-form');
+
+    expect(form).to.be.accessible();
+  })
 });
